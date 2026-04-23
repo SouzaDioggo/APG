321856a0d7cb03814851b9fbf75aa3b67b7ab1f9
Vinícius <viniciusdiller@hotmail.com>
2026-04-23 15:15:42 -0300

    refactor: limpa de componentes inúteis

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import { HeroSection } from "@/components/landing-page/hero-section";
-import { AboutSection } from "@/components/landing-page/about-section";
-import { FeatureBanner } from "@/components/landing-page/feature-banner";
-import { CoursesSection } from "@/components/landing-page/courses-section";
 import { BenefitsSection } from "@/components/landing-page/benefits-section";
-import { TestimonialsSection } from "@/components/landing-page/testimonials-section";
-import { FounderSection } from "@/components/landing-page/founder-section";
 import { ClientsSection } from "@/components/landing-page/clients-section";
-import { LatestArticles } from "@/components/landing-page/latest-articles";
 import { CTASection } from "@/components/landing-page/cta-section";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
@@ -20,23 +14,14 @@ export default function Home() {
       <Navbar />
       <HeroSection />
 
-      {/* As seções abaixo só animam quando você rolar até elas */}
-
       <ScrollReveal>
         <BenefitsSection />
       </ScrollReveal>
-
-      {/* { <FeatureBanner /> }  */}
-      {/* { </ScrollReveal>} } */}
 
       {/* Seção: Para Quem Atendemos */}
       <ScrollReveal>
         <ClientsSection />
       </ScrollReveal>
-
-      {/* <ScrollReveal>
-        <TestimonialsSection />
-      </ScrollReveal> seção de depoimentos */}
 
       {/* CTA Final */}
       <ScrollReveal>
